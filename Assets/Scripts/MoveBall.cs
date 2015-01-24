fe205b9ed4957583ec2e2ffd7c2f790435f93b18
gurshane <gurshane@ymail.com>
2015-01-24 00:07:38 -0500

    Completed Basic Terrain, Camp, and Clearings
    -Completed terrain look
    -Completed Ammo and Health Clearings
    -Completed Camp and decor
    -Completed Bamboo barrier

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate()
+	{
+
+		float moveHor = Input.GetAxis ("Horizontal");
+		float moveVer = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVer);
+
+		rigidbody.AddForce (movement * speed );
+
+	}
+
+}
